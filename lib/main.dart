ffc14918ce75fbd559981190dc61cf9e47176dd5
tixs <995975102@qq.com>
2020-04-14 15:17:58 +0800

    login页面
    login有点bug，字挡住了

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:handsound/home_page.dart';
+import 'package:handsound/user_provider.dart';
+
+
+
+void main() {
+//  debugPaintSizeEnabled=true;
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'TheGorgeousLogin',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:
+      /**
+       * 将用户数据共享给子控件，任何地方的子控件都可以获取到父控件所保存的用户信息
+       * 根据有没有用户信息，进入不同的页面
+       */
+//      new UserContainer(user:new User("冬瓜","123456"),child: new HomePage(),),
+      new UserContainer(user: null, child: new HomePage(),
+      ),
+    );
+  }
+}
