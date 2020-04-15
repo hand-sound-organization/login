7e24ff031de107ed0892e9306330150e55c12bcc
tixs <995975102@qq.com>
2020-04-15 22:58:53 +0800

    wifi搜索
    wifi搜索

---
+++ lib/lock_binding/bingding_search_page.dart
@@ -0,0 +1,341 @@
+import 'dart:io';
+import 'dart:async';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+import 'package:handsound/login_page.dart';
+import 'package:handsound/sign_in_page.dart';
+import 'bloc/theme.bloc.dart';
+import 'configs/themes.dart';
+import 'package:handsound/stayfit-health-clone/progress_bar.dart';
+import 'package:handsound/user_provider.dart';
+import 'package:handsound/user.dart';
+
+class SearchPage extends StatefulWidget {
+  SearchPage({Key key}) : super(key: key);
+
+  _SearchPage createState() => _SearchPage();
+}
+
+class _SearchPage extends State<SearchPage> {
+
+
+  @override
+  void initState() {
+    super.initState();
+    themeBloc.changeTheme(Themes.stayfit);
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                Image.network(
+                  'https://www.debuda.net/wp-content/uploads/2017/11/como-decorar-una-habitacion-para-meditar.jpg',
+                  fit: BoxFit.cover,
+                ),
+                DecoratedBox(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color(0xFF422479).withOpacity(0.95),
+                        Color(0xFF1C0746).withOpacity(0.75),
+                      ],
+                      begin: FractionalOffset.topCenter,
+                      end: FractionalOffset.bottomCenter,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 40,
+                  width: size.width,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: 65,
+                          alignment: Alignment.centerLeft,
+                          child: Icon(
+                            Icons.menu,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          '门锁注册',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                          ),
+                        ),
+                        Row(
+                          children: <Widget>[
+//                            Stack(
+//                              children: <Widget>[
+//                                Container(
+//                                  width: 50,
+//                                  child: Icon(
+//                                    Icons.new_releases,
+//                                    size: 30,
+//                                    color: Colors.white,
+//                                  ),
+//                                ),
+//                                Positioned(
+//                                  top: 0,
+//                                  right: 0,
+//                                  child: Container(
+//                                    height: 20,
+//                                    width: 20,
+//                                    alignment: Alignment.center,
+//                                    child: Text(
+//                                      '7',
+//                                      style: TextStyle(
+//                                        color: Colors.white,
+//                                        fontWeight: FontWeight.bold,
+//                                      ),
+//                                    ),
+//                                    decoration: BoxDecoration(
+//                                      color: Colors.blue,
+//                                      borderRadius: BorderRadius.circular(50),
+//                                    ),
+//                                  ),
+//                                )
+//                              ],
+//                            ),
+                            SizedBox(width: 10),
+                            Icon(
+                              Icons.edit,
+                              color: Colors.white,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  width: size.width,
+                  height: size.height - size.height * .3,
+                  child: Stack(
+
+                    fit: StackFit.loose,
+                    children: <Widget>[
+                      new FlareActor(
+                        "assets/wifi.flr",
+                        alignment: Alignment.center,
+                        fit: BoxFit.none,
+                        animation: "wifi",
+                      ),
+//                      Container(
+//                          width: size.width,
+//                          height: size.height - size.height * .28,
+//                          alignment: Alignment.center,
+//                          child:
+//                          new Image(image: AssetImage("assets/sound.png"))
+////                        Icon(
+////                          Icons.favorite,
+////                          size: 100,
+////                          color: Colors.black,
+////                        ),
+//                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 100,
+                  left: 40,
+                    child:Card(
+                      color: Colors.blueAccent,
+                      //z轴的高度，设置card的阴影
+                      elevation: 20.0,
+                      //设置shape，这里设置成了R角
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(5.0),
+                            topRight: Radius.circular(40.0),
+                            bottomLeft: Radius.circular(5.0),
+                            bottomRight: Radius.circular(5.0)),
+                      ),
+                      //对Widget截取的行为，比如这里 Clip.antiAlias 指抗锯齿
+                      clipBehavior: Clip.antiAlias,
+                      semanticContainer: false,
+                      child: getChild(),
+                    )
+                ),
+              ],
+            ),
+          ),
+
+
+//          Container(
+//            width: size.width,
+//            height: size.height * .28,
+//            child: Row(
+//              children: <Widget>[
+//                _buildDashboardItem(
+//                  Color(0xFF23BFFF),
+//                  Icons.local_drink,
+//                  70,
+//                  '0.58',
+//                  'LTRS',
+//                  'DRINK',
+//                ),
+//                _buildDashboardItem(
+//                  Color(0xFF9CDD5D),
+//                  Icons.fastfood,
+//                  50,
+//                  '458',
+//                  'GRMS.',
+//                  'FOOD',
+//                  true,
+//                ),
+//                _buildDashboardItem(
+//                  Color(0xFFEF453C),
+//                  Icons.hotel,
+//                  30,
+//                  '7.30',
+//                  'HRS',
+//                  'SLEEP',
+//                ),
+//              ],
+//            ),
+//          ),
+
+
+        ],
+      ),
+    );
+
+
+
+
+  }
+
+  Widget _buildDashboardItem(Color color, IconData icon,
+      double completedPercentage, String value, String unit, String name,
+      [bool useLight = false]) {
+    List<Color> colors = [
+      Color(0xFF311b5b),
+      Color(0xFF221441),
+    ];
+
+    if (useLight) {
+      colors = [
+        Color(0xFF311b5b),
+        Color(0xFF321c5c),
+      ];
+    }
+
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: colors,
+            begin: FractionalOffset.topLeft,
+            end: FractionalOffset.bottomRight,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: CustomPaint(
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          value,
+                          style: TextStyle(
+                            color: color,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 23,
+                          ),
+                        ),
+                        SizedBox(height: 2),
+                        Text(
+                          unit,
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  foregroundPainter: ProgressPainter(
+                    percentageCompletedCircleColor: color,
+                    completedPercentage: completedPercentage,
+                    circleWidth: 10.0,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 60,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    icon,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    name,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+
+
+
+  getChild() {
+    return Container(
+
+      color: Colors.white,
+      width:300,
+      height: 140,
+      alignment: Alignment.center,
+
+      child: Text(
+        "Card",
+        style: TextStyle(fontSize: 28, color: Colors.grey),
+      ),
+    );
+  }
+}
+
+
